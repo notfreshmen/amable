270688b357a821ba9061d92880acdfb3f1744776
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2016-09-09 23:37:13 -0400

    load config from file.

--- psn/__init__.py
+++ psn/__init__.py
@@ -1,8 +1,22 @@
+from os.path import join
+from os.path import dirname
+
+from dotenv import load_dotenv
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 from psn.blueprints.base import base
 
 
+load_dotenv(join(dirname(__file__), '..', '.env'))
+
 app = Flask(__name__)
+app.config.from_envvar('PSN_SETTINGS')
+
+
+from models import db
+from models.user import User
+
 
 app.register_blueprint(base)
