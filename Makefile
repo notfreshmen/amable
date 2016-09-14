774eb8e997a7f0ceeafef4ff2a5eec78a2df9b04
maxbbender <maxbbender@gmail.com>
2016-09-13 23:28:33 -0400

    started to build user table

--- Makefile
+++ Makefile
@@ -7,6 +7,14 @@ install:
 	pip install --upgrade pip
 	pip install -r requirements.txt
 
+installLin:
+	sudo apt-get -y install postgresql
+	sudo apt-get -y install python-psycopg2
+	sudo apt-get -y install libpq-dev
+	sudo apt-get -y build-dep python-psycopg2
+	pip install --upgrade pip
+	pip install -r requirements.txt
+
 console:
 	PYTHONSTARTUP=./console.py python
 
@@ -23,6 +31,6 @@ coverage:
 	coverage report
 
 db_setup:
-	createuser -U postgres -h localhost -p 5432 amable
-	createdb -h localhost -p 5432 amable_development
+	createuser -U postgres -h localhost -p 5432 -d -P amable # -createDB|-PromptPWD
+	createdb -U amable -h localhost -p 5432 amable_development
 	python db/manage.py version_control
--- models/user.py
+++ models/user.py
@@ -5,10 +5,29 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(128))
+    salt = db.Column(db.String(128))
+    name = db.Column(db.String(128))
+    role = db.Column(db.String(10))
+    bio = db.Column(db.Text)
+    website = db.Column(db.String(128))
+    location = db.Column(db.String(128))
+    phone = db.Column(db.String(10))
+    dob = db.Column(db.DateTime)
+    profile_image = db.Column(db.String(128))
+    date_created = db.Column(db.String(128))
+    date_modified = db.Column(db.String(128))
 
-    def __init__(self, username, email):
+    def __init__(self, username, email, password, name, role=None, bio, website, location, phone, dob, profile_image=None):
         self.username = username
         self.email = email
+
+        randString = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(20))
+
+
+
+
+
 
     def __repr__(self):
         return '<User %r>' % self.username
