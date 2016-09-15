38de02026212684abd00b54e01667eb69311e767
Max Bender <maxbbender@gmail.com>
2016-09-14 20:25:15 -0400

    updates to userpy

--- models/user.py
+++ models/user.py
@@ -1,5 +1,9 @@
 from amable import *
-
+from amable import util
+from datetime import datetime
+from sqlalchemy import event
+import hashlib
+import uuid
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -18,16 +22,48 @@ class User(db.Model):
     date_created = db.Column(db.String(128))
     date_modified = db.Column(db.String(128))
 
-    def __init__(self, username, email, password, name, role=None, bio, website, location, phone, dob, profile_image=None):
+    def __init__(self, username, email, password, name, bio, website, location, phone, dob, profile_image=None, role=None):
         self.username = username
         self.email = email
 
-        randString = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(20))
+        hashedPassword = util.hash_password(password) # Hash the password. SHA256
+        splitPassword = hashedPassword.split(":") # Split the password and the salt
 
+        self.password = splitPassword[0]
+        self.salt = splitPassword[1]
 
+        self.name = name
+        
+        if role is not None:
+            self.role = role
+        else:
+            self.role = "user"
 
+        self.bio = bio
+        self.website = website
+        self.location = location
+        self.phone = phone
+        self.dob = dob
+
+        if profile_image is not None:
+            self.profile_image = profile_image
+        else:
+            self.profile_image = ""
+
+        now = datetime.datetime(year, month, day, hour, minute, second)
+
+        self.date_created = now
+        self.date_modified = now
 
 
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+
+    def after_insert_listener(mapper, connection, target):
+        # 'target' is the inserted object
+        target.date_modified = datetime.datetime(year, month, day, hour, minute, second)
+
+    event.listen(self, 'after_insert', after_insert_listener)
