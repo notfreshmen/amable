304ef32fc1ba1af7820b0a944a0a3bff243db49a
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2016-09-27 19:32:56 -0400

    remove gulp, use flask-assets.

--- amable/blueprints/base.py
+++ amable/blueprints/base.py
@@ -1,9 +1,15 @@
 from flask import Blueprint
+from flask import render_template
 
 
-base = Blueprint('base', __name__, template_folder='templates')
+base = Blueprint('base', __name__, template_folder='../templates/base')
 
 
 @base.route('/')
 def index():
     return "Hello world"
+
+
+@base.route('/ui')
+def ui():
+    return render_template('ui.html')
