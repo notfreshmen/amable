7109adb923a55f8f6d6050390a7f678b6771bc9b
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2016-09-27 20:05:50 -0400

    added tests for assets.

---
+++ spec/amable/utils/assets_spec.py
@@ -0,0 +1,17 @@
+from expects import *
+
+from amable.utils.assets import assets_path, sass_filter
+
+
+with context('amable'):
+    with context('utils'):
+        with context('assets'):
+            with context('assets_path'):
+                with it('finds the right directory'):
+                    correct_dir = '/amable/utils/../assets/css'
+
+                    expect(assets_path('css')).to(contain(correct_dir))
+
+            with context('sass_filter'):
+                with _it('takes SCSS from stdin and outputs CSS to stdout'):
+                    pass
