47f026f19d4a4de5dcf77eb93266d3fc02bd1927
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2016-10-12 08:44:11 -0400

    test node_modules_path().

--- spec/amable/utils/assets_spec.py
+++ spec/amable/utils/assets_spec.py
@@ -1,6 +1,6 @@
 from expects import *
 
-from amable.utils.assets import assets_path, sass_filter
+from amable.utils.assets import assets_path, node_modules_path, sass_filter
 
 
 with context('amable'):
@@ -11,6 +11,12 @@ with context('amable'):
                     correct_dir = '/amable/utils/../assets/css'
 
                     expect(assets_path('css')).to(contain(correct_dir))
+
+            with context('node_modules_path'):
+                with it('finds the right directory'):
+                    correct_dir = '/amable/utils/../../node_modules/jquery'
+
+                    expect(node_modules_path('jquery')).to(contain(correct_dir))
 
             with context('sass_filter'):
                 with _it('takes SCSS from stdin and outputs CSS to stdout'):
