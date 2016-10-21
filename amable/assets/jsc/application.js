7ea46e675587cabeb4ed82aea5e5f240bd421b6d
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2016-10-20 21:59:59 -0400

    fixed js linting issues.

--- amable/assets/jsc/application.js
+++ amable/assets/jsc/application.js
@@ -1,10 +1,8 @@
-$(function() {
-
+$(function () {
   $('.header__toggle').on('click', function () {
     var nav = $(this).siblings('.header__nav')
 
     $(nav).toggleClass('mobile-hidden')
     $(nav).toggleClass('mobile-visible')
   })
-
 })
