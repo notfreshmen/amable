b5d19d25c49e938af025ee8521491abeb517ba46
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2016-10-23 15:30:17 -0400

    fix vagrant installation.

--- cfg_vagrant/sql_script.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE amable_development;
-GRANT ALL PRIVILEGES ON DATABASE amable_development to amable;
-CREATE DATABASE amable_test;
-GRANT ALL PRIVILEGES ON DATABASE amable_test to amable;
