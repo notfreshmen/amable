a184e2dad56e6821834ecd3d106d786ea980b01e
Max Bender <maxbbender@gmail.com>
2016-10-25 12:31:32 -0400

    password checks

--- amable/utils/password.py
+++ amable/utils/password.py
@@ -10,8 +10,15 @@ def hash_password(password):
         password.encode()).hexdigest() + ':' + salt
 
 
-def check_password(hashed_password, user_password):
-    password, salt = hashed_password.split(':')
-    return password == hashlib.sha256(
-        salt.encode() +
-        user_password.encode()).hexdigest()
+# def check_password(hashed_password, user_password):
+#     password, salt = hashed_password.split(':')
+#     return password == hashlib.sha256(
+#         salt.encode() +
+#         user_password.encode()).hexdigest()
+
+def check_password(User, user_input_password):
+	salt = User.salt
+	hashed_password_to_check = hashlib.sha256(salt.encode() + user_input_password.encode())
+
+	return User.password == hashed_password_to_check
+
