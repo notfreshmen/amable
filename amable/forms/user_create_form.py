bd8de26f0bf8b9280582d612d4a58136b7f93e98
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2016-10-25 23:50:58 -0400

    test user create form.

--- amable/forms/user_create_form.py
+++ amable/forms/user_create_form.py
@@ -1,9 +1,10 @@
 from wtforms import Form, StringField, PasswordField, validators
 from wtforms.fields.html5 import EmailField
 
+
 class UserCreateForm(Form):
     username = StringField('Username', [validators.Required(), validators.Length(min=3, max=25)])
     email = EmailField('Email', [validators.Required(), validators.Email()])
     name = StringField('Name', [validators.Required()])
-    password = PasswordField('Password', [validators.Required(), validators.EqualTo('password_confirmation', message='Passwords must match')])
+    password = PasswordField('Password', [validators.Required(), validators.EqualTo('password_confirmation', message='Passwords must match.')])
     password_confirmation = PasswordField('Password confirmation', [validators.Required()])
---
+++ spec/amable/forms/user_create_form_spec.py
@@ -0,0 +1,59 @@
+from expects import *
+
+from amable.forms.user_create_form import UserCreateForm
+
+
+with context('amable.forms.user_create_form'):
+    with context('UserCreateForm'):
+        with context('username'):
+            with it('is required'):
+                form = UserCreateForm()
+                form.validate()
+
+                expect(form.errors['username']).to(contain("This field is required."))
+
+            with it('must be between 3 and 25 characters'):
+                form = UserCreateForm(username='kk')
+                form.validate()
+
+                expect(form.errors['username']).to(contain("Field must be between 3 and 25 characters long."))
+
+        with context('email'):
+            with it('is required'):
+                form = UserCreateForm()
+                form.validate()
+
+                expect(form.errors['email']).to(contain("This field is required."))
+
+            with it('must be a valid email'):
+                form = UserCreateForm(email='foobar')
+                form.validate()
+
+                expect(form.errors['email']).to(contain("Invalid email address."))
+
+        with context('name'):
+            with it('is required'):
+                form = UserCreateForm()
+                form.validate()
+
+                expect(form.errors['name']).to(contain("This field is required."))
+
+        with context('password'):
+            with it('is required'):
+                form = UserCreateForm()
+                form.validate()
+
+                expect(form.errors['password']).to(contain("This field is required."))
+
+            with it('must match password_confirmation'):
+                form = UserCreateForm(password='foobar', password_confirmation='barfoo')
+                form.validate()
+
+                expect(form.errors['password']).to(contain("Passwords must match."))
+
+        with context('password_confirmation'):
+            with it('is required'):
+                form = UserCreateForm()
+                form.validate()
+
+                expect(form.errors['email']).to(contain("This field is required."))
