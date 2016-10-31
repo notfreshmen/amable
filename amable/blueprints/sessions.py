fd13d1e1e8e36a142bda6660b85890e856524010
maxbbender <maxbbender@gmail.com>
2016-10-31 15:01:00 -0400

    login complete

--- amable/blueprints/sessions.py
+++ amable/blueprints/sessions.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, request, flash
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 
 from amable.models.user import User
 from amable import session, login_manager
@@ -24,34 +24,44 @@ def login():
     return render_template('login.html', form=login_form)
 
 
+@sessions.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return redirect('/')
+
+
 @sessions.route('/sessions/create', methods=['POST'])
 def create():
     # email 'variable' is the column. request.form['email'] is the post data
-    print("email provided : " + request.form['email'])
-    user = session.query(User).filter_by(email=request.form['email']).first()
+    # print("email provided : " + request.form['email'])
 
-    # Does the user exist from the query done above?
-    if user is None:
-        flash("No User Found")
-        return redirect('/login')
-    else:
         # testing
-        print("create() ~ User Password : " + user.password)
-        print("create() ~ Username : " + user.username)
+        # print("create() ~ User Password : " + user.password)
+        # print("create() ~ Username : " + user.username)
 
     form = LoginForm()
 
     if form.validate_on_submit():
-        print("Form validated")
-        if check_password(user, request.form["password"]):
-            print("password checked good to go")
-            login_user(user)
-            flash("Login Successful")
-            return redirect("/")
-        else:
-            flash("Login Credentials are wrong")
+        # Is the form of valid format?
+        user = session.query(User).filter_by(
+            email=request.form['email']).first()
+
+        # Does the user exist from the query done above?
+        if user is None:
+            flash("No User Found")
             return redirect('/login')
+        else:
+            # print("Form validated")
+            if check_password(user, request.form["password"]):
+                # print("password checked good to go")
+                login_user(user)
+                # flash("Login Successful")
+                return redirect("/")
+            else:
+                flash("Error validating login credentials, please try again")
+                return redirect('/login')
     else:
         flash_errors(form)
-        flash("Login format wrong")
+        flash("Error validating login format, please try again")
         return redirect('/login')
