8e5f6d185ff7c3f66f658bf70ff0ab6ba6c689d9
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2016-11-08 13:47:05 -0500

    add permalink column to communities. generate a permalink if not given one.

---
+++ db/versions/027_add_permalink_to_communities.py
@@ -0,0 +1,16 @@
+from sqlalchemy import *
+from migrate import *
+
+
+permalink_column = Column('permalink', String(128))
+
+def upgrade(migrate_engine):
+    meta = MetaData(bind=migrate_engine)
+
+    communities = Table('communities', meta, autoload=True)
+
+    permalink_column.create(communities)
+
+
+def downgrade(migrate_engine):
+    permalink_column.drop()
