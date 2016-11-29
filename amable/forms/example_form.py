5318eb5daf327720765c4cad5626e25d5714edc9
maxbbender <maxbbender@gmail.com>
2016-11-29 15:52:12 -0500

    lint fix

--- amable/forms/example_form.py
+++ amable/forms/example_form.py
@@ -3,5 +3,6 @@ from flask_wtf import FlaskForm
 # from wtforms import IntegerField, FileField, BooleanField, TextAreaField, validators
 # from wtforms.fields.html5 import EmailField, DateField
 
+
 class XXXXForm(FlaskForm):
     xxx = StringField('XXX')
